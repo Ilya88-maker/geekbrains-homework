956321e3dee2dcc099f159918df5f64ea37548cc
ilya88-maker <inadezhkin88@gmail.com>
2021-03-23 17:00:05 +0300

    Создал класс, который заполняет поля при создании объекта

--- src/ru/geekbrains/homework5/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.homework5;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-
-}
---
+++ src/ru/geekbrains/lesson5/Employee.java
@@ -0,0 +1,30 @@
+package ru.geekbrains.lesson5;
+
+public class Employee {
+
+    public String FullName;
+    public String Position;
+    public int number;
+    public int salary;
+    public int age;
+
+    public Employee(String FullName, String Position, int number, int salary, int age ){
+
+        this.FullName = FullName;
+        this.Position = Position;
+        this.number = number;
+        this.salary = salary;
+        this.age = age;
+
+    }
+
+    public Employee() {
+
+    }
+
+    public void createInformation(String[] args) {
+
+        Employee Employee1 = new Employee();
+
+    }
+}
