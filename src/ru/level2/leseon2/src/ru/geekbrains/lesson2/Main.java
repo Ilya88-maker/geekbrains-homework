5ff91c9f8876df541647d0c50e5598db7351c233
ilya88-maker <inadezhkin88@gmail.com>
2021-04-06 19:12:27 +0300

    Создал массив 4x4

--- src/ru/level2/leseon2/src/ru/geekbrains/lesson2/Main.java
+++ src/ru/level2/leseon2/src/ru/geekbrains/lesson2/Main.java
@@ -3,6 +3,14 @@ package ru.geekbrains.lesson2;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int counter = 1;
+        int[][] table = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                table[i][j] = counter;
+
+
+            }
+        }
     }
 }
