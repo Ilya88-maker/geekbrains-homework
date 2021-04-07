e2c1141b971047d2deabb0260c073ed832156190
ilya88-maker <inadezhkin88@gmail.com>
2021-04-07 21:55:18 +0300

    Сделал 3 задания

--- src/ru/level2/leseon2/src/ru/geekbrains/lesson2/Main.java
+++ src/ru/level2/leseon2/src/ru/geekbrains/lesson2/Main.java
@@ -3,14 +3,53 @@ package ru.geekbrains.lesson2;
 public class Main {
 
     public static void main(String[] args) {
-        int counter = 1;
-        int[][] table = new int[4][4];
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                table[i][j] = counter;
 
-
+        String[][] arr = new String[][]
+                {{"1", "1", "1", "1"},
+                 {"2", "2", "2", "2"},
+                 {"3", "3", "3", "3"},
+                 {"4", "4", "4", "4"}};
+        try {
+            try {
+                int result = method(arr);
+                System.out.println(result);
+            } catch (MyArraySizeException e) {
+                System.out.println("Размер массива превышен!");
             }
         }
+        catch (MyArrayDataException e) {
+            System.out.println("Неправильное значение массива!");
+            System.out.println("Ошибка в ячейке: " + e.i + "x" + e.j);
+        }
+
     }
+
+
+    public static int method(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        int count = 0;
+        if (arr.length != 4) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraySizeException();
+            }
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    count = count + Integer.parseInt(arr[i][j]);
+                }
+                catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+
+        }
+        return count;
+    }
+
 }
+
+    
+    
+
+
---
+++ src/ru/level2/leseon2/src/ru/geekbrains/lesson2/MyArraySizeException.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.lesson2;
+
+public class MyArraySizeException extends Exception {
+    MyArraySizeException(String msg) {
+        super(msg);
+    }
+
+    public MyArraySizeException() {
+
+    }
+}
