552e4898a445e14eacc7fb8dfefb83b9bbdee54c
ilya88-maker <inadezhkin88@gmail.com>
2021-04-14 22:29:33 +0300

    Вывод количество повторяющихся слов

--- src/ru/level2/lesson3/src/ru/geekbrains/lesson3/Main.java
+++ src/ru/level2/lesson3/src/ru/geekbrains/lesson3/Main.java
@@ -1,8 +1,25 @@
 package ru.geekbrains.lesson3;
 
-public class Main {
+import java.util.*;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main {
+    public static void main(String[] args){
+        List<String> words = Arrays.asList(
+                "Mercedes", "BMW", "Audi", "Bently", "Volkswagen",
+                "Opel", "Subaru", "Volvo", "Bently", "Volkswagen",
+                "BMW", "Audi", "Bently", "Subaru", "Subaru",
+                "Audi", "Bently", "Subaru", "Subaru", "Bently"
+        );
+
+        Set<String> unique = new HashSet<String>(words);
+
+        System.out.println("Первоначальный массив");
+        System.out.println(words.toString());
+        System.out.println("Уникальные слова");
+        System.out.println(unique.toString());
+        System.out.println("Частота встречаемости слов");
+        for (String key : unique) {
+            System.out.println(key + ": " + Collections.frequency(words, key));
+        }
     }
 }
