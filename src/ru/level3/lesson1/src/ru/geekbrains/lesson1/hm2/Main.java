1196514c07fec212ca7c7499f2f6895935f23ced
ilya88-maker <inadezhkin88@gmail.com>
2021-04-28 23:27:13 +0300

    Level3

---
+++ src/ru/level3/lesson1/src/ru/geekbrains/lesson1/hm2/Main.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.lesson1.hm2;
+
+public class Main {
+}
---
+++ src/ru/level3/lesson1/src/ru/geekbrains/lesson1/hm2/Rename.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.lesson1.hm2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Rename {
+
+
+    public static void main(String[] args) {
+        Integer[] array = {1, 3, 5};
+        List<Integer> list = Arrays.asList(array);
+
+    }
+}
